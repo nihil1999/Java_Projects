7e187326eb2b81402062bd778b271bbb9ff2603a
“Nihil” <nihilrama@gmail.com>
2023-03-12 23:52:07 +0530

    Guess a number game

--- projects/GuessTheNumber_Game.java
+++ projects/GuessTheNumber_Game.java
@@ -3,13 +3,15 @@
  */
 package projects;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 /**
  * @author logan
  * 
- * A game to guess random number, it will show the correct number with same position as (c), number presents but position different as (p), wrong number as (x).
+ * A game to guess random number, it will show the correct number with same position as (c), 
+ * number presents but position different as (p), wrong number as (x).
  */
 public class GuessTheNumber_Game {
 
@@ -18,7 +20,7 @@ public class GuessTheNumber_Game {
 	 */
 	
 	Scanner sc;
-	int random_arr[];
+	int random_arr[]; //= {1,2,3,4};
 	int user_arr[];
 	int count_random[];
 	int count_user[];
@@ -30,42 +32,86 @@ public class GuessTheNumber_Game {
 	{
 		/* 
 		 * For guess the number with different range that can user guess. The user can select digit range of a number to guess.
-		 * Here i used Random class's nextInt(int bound) method to generate random number, this will generate random number upon the bound range.
-		 * Every time the random class's method called it will generate single number, so i have called the method in for loop, it will generate number depends upon the range of digit that user gives.
+		 * Here i used Random class's nextInt(int bound) method to generate random number, 
+		 * this will generate random number upon the bound range.
+		 * Every time the random class's method called it will generate single number, 
+		 * so i have called the method in for loop, it will generate number depends upon the range of digit that user gives.
 		 * I have stored the random number that generates each time in an array to check with user given number on later.
 		 * I have also stored the random number in String to use that anywhere without loop, bcauz random number stored in array.
-		 * At last i have called toCount_Random() method to count the appearance of random numbers and stored in an count_random[] array, to check the user given number's appearance on later. 
+		 * At last i have called toCount_Random() method to count the appearance of random numbers and 
+		 * stored in an count_random[] array, to check the user given number's appearance on later. 
 		 * */
 		
 		sc=new Scanner(System.in);
 		System.out.println("Enter the digit range of number that you want to guess:");
-		range=sc.nextInt();
-		
-		Random r=new Random();
-		random_arr=new int[range];
-		random_number="";				
-		
-		for(int i=0;i<range;i++)
+		try 
 		{
-			random_arr[i]=r.nextInt(9);		
-			random_number+=random_arr[i];
+			range=sc.nextInt();
+			
+			if(range<=0)
+			{
+				throw new GuessNumberException(Error_msg_GuessNumber.err5_range_low_0);
+			}
+			if(range>10)
+			{
+				throw new GuessNumberException(Error_msg_GuessNumber.err4_range_restrict);
+			}
+			
+			Random r=new Random();
+			random_arr=new int[range];
+			random_number="";				
+			
+			for(int i=0;i<range;i++)
+			{
+				random_arr[i]=r.nextInt(9);		
+				random_number+=random_arr[i];
+			}
+			
+			/* !$ user notice to test: $!
+			 * if any one want to test their number please comment above all upto range=sc.nextInt and 
+			 * give your value in instancely created int random_arr[] above this method like int random_arr[]={1,2,3,4}
+			 * and use below range, for loop to display random number. Also comment GuessNumberException catch block.  */
+
+				//			range=random_arr.length;		
+				//			for(int i:arr1)
+				//				System.out.print(i+" ");
+			
+			toCount_Random();
 		}
-		
-//		for(int i:arr1)
-//			System.out.print(i+" ");
-		
-		toCount_Random();
-		
+		catch(InputMismatchException e)
+		{
+			e=new InputMismatchException(Error_msg_GuessNumber.err2_input_mismatch);
+			System.out.println(e.getMessage());
+			toGetRandom_Number();
+		}
+		catch (GuessNumberException e) 
+		{
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			toGetRandom_Number();
+		}
+		catch (Exception e) 
+		{
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			
+		}
 	}
 	
 	public void toGetUser_Number()
 	{
-		/* The user will give the number to guess and it will be stored as string, bcauz java doesn't consider 0 if a number starts with 0 in int, and handled exception that if user gives character it displays some message.
-		 * The number in a string will be stored in an array to check with random number, if user gives number less than range then it will display some message.
-		 * To store number in array, i need to directly store each number in array than to store in integer variable, bcauz if number starts with 0, integer won't consider 0.
-		 * For that i have taken string as char by charAt and stored in integer variable, now the ASCII value of char will be stored in integer so it'll take 0. Now i stored that ASCII number - 48 (number starts with 48 in ASCII, so for 48 (0)-48 : 0 ) and stored in array,   
+		/* The user will give the number to guess and it will be stored as string,
+		 * because java doesn't consider 0 if a number starts with 0 in integer, 
+		 * and handled exception that if user gives character it displays some message.
+		 * The number in a string will be stored in an array to check with random number, 
+		 * if user gives number less than range then it will display some message.
+		 * To store number in array, i need to directly store each number in array than to store in integer variable, 
+		 * Because if number starts with 0, integer won't consider 0.
+		 * For that i have taken string as char by charAt and stored in integer variable, 
+		 * now the ASCII value of char will be stored in integer so it'll take 0. 
+		 * Now i stored that ASCII number - 48 (number starts with 48 in ASCII, so for 48 (0)-48 : 0 ) and stored in array,   
 		 * For e.g user given number in string ->123 => ASCII : 49,50,51; a[0]=49-48 : 1, a[1]=50-48: 2, a[2]=51-48 : 3.
-		 * Here i called guessnumber() method to check random number with user number.  
+		 * Here i called toGuessNumber() method to check random number with user giving number.  
 		 *   */
 		
 		sc=new Scanner(System.in);
@@ -85,7 +131,8 @@ public class GuessTheNumber_Game {
 			
 			if(str.length()!=range)
 			{
-				throw new GuessNumberException(Error_msg_GuessNumber.err1_input_mismatch);
+				
+				throw new GuessNumberException(Error_msg_GuessNumber.err1_range_mismatch);
 			}
 	
 			for(int i=0;i<str.length();i++)
@@ -103,7 +150,7 @@ public class GuessTheNumber_Game {
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
-			e=new ArrayIndexOutOfBoundsException(Error_msg_GuessNumber.err1_input_mismatch);
+			e=new ArrayIndexOutOfBoundsException(Error_msg_GuessNumber.err1_range_mismatch);
 			System.out.println(e.getMessage());
 			toGetUser_Number();
 		} 
@@ -116,13 +163,35 @@ public class GuessTheNumber_Game {
 		catch (Exception e) 
 		{
 			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
-			
+			System.out.println(e.getMessage());	
 		}		
 	}
 	
   public void toGuessNumber()
 	{
+		/* First i have taken char array, to mark the characters (c,x,p ) 
+		 * i.e. whether the number is correct or wrong and correct position or wrong position. 
+		 * Initially i have checked the user given number with random number on position or index based, 
+		 * for e.g => random number = 1212 , user given number = 1111 , 
+		 * i'll check user given number's first position with random number's first position and 
+		 * next user given number's second position with random number's second position and it goes....
+		 * and i have used variable(check) to count, i.e if both the number are in same position and correct 
+		 * then it'll increase its value by 1, and i have also counted the user given number in count_user array.
+		 * so atlast if check is equal to range(random number's digit) then all numbers user given is correct, so it'll end there.
+		 * if not, then it enter into else block where i given if the current index has character = 'c' then it will move to next number.
+		 * Because i have already checked correct number with same position and i have the count of occurrence of user given number.
+		 * now i have to check whether the number presents but with different position or not presents.
+		 * for that i compare the user given number's occurrence that doesn't have character ='c' with the user given number's 
+		 * occurrence in random number, if the user given number's occurrence count greater then the number not presents otherwise 
+		 * the number presents but in different position.
+		 * For e.g => random number = 1212 , user given number = 1111, after first logic (c, ,c, ) 1st and 3rd position and number are same 
+		 * so it marked as 'c' and count of occurrence are added, count of occurrence of 1 (2).
+		 * other's aren't same so it doen't marked, to check if the number presents in different position or not presents.
+		 * so in 2nd logic 1st position and 3rd position number willn't enter 2nd number will enter and counts the occurrence of that number
+		 * and after it compares now user number occurrence is (3) which is greater than random number's occurrence of 1 (2)
+		 * so it will mark as 'x'.If no user given number presents in random number then it'll return counts as 0. 
+		 * if occurrence count is lesser then the number presents but in different position, so it'll mark as 'p'.
+		 */
 	  
 		char ch[]=new char[range];
 		int check=0;
@@ -130,15 +199,12 @@ public class GuessTheNumber_Game {
 		
 		for(int i=0;i<user_arr.length;i++)
 		{
-			for(int j=i;j<=i;j++)
-			{
-				if(user_arr[i]==random_arr[j])
+			if(user_arr[i]==random_arr[i])
 				{
 					ch[i]='c';
 					check++;
-					toUser_Count(user_arr[i],count_user);
+					toUser_Count(user_arr[i]);
 				}
-			}
 		}
 		
 		if(check==range)
@@ -153,41 +219,19 @@ public class GuessTheNumber_Game {
 		else
 		{
 			for(int i=0;i<user_arr.length;i++)
-			{
-					int j=0,index=0;
-					boolean value=false;
-	
+			{	
 				if(ch[i]!='c') 
 				{
-					
-					toUser_Count(user_arr[i],count_user);
-					
-					while(index<i)				//to check if the number is presents before, if it presents then the outer loop(j) will start from (i)index.
-					{								//the loop will run until i index. 
-						if(user_arr[i]==user_arr[index] && ch[index]=='p')
-							{
-							if(count_user[toFindNumber(user_arr[i])]>count_random[toGetCount(user_arr[i])])
-								{
-									j=random_arr.length;
-								}
-							}
-						index++;
-					}
-					
-				for(;j<random_arr.length;j++)
-				{
-					
-					if(user_arr[i]==random_arr[j] && ch[j]!='c')				//if user number and random number are same but their index is different, then it will indicate as p.
-					{
-						ch[i]='p';
-						value=true;
-						break;
-					}
-				}
-				
-				if(j==random_arr.length && value!=true)							//if user number not same as random number, then it will indicate as x.
-					ch[i]='x';
-				
+						toUser_Count(user_arr[i]);
+						
+						if(toFindcount(user_arr[i],user_arr,count_user) > toFindcount(user_arr[i],random_arr,count_random))
+						{
+							ch[i]='x';
+						}
+						else
+						{
+							ch[i]='p';
+						}	
 				}
 			}
 			
@@ -199,17 +243,8 @@ public class GuessTheNumber_Game {
 				toview++;
 				if(toview%10==0)
 				{
-					System.out.println("Do you want to reveal the number: \n1. Yes 2. To continue guessing");
-					int choice=sc.nextInt();
-					switch(choice)
-					{
-						case 1:
-							System.out.println(random_number);
-							break;
-						case 2:
-							toGetUser_Number();
-							break;
-					}
+					System.out.println("\n\nDo you want to reveal the number: \n1. Yes \n2. To continue guessing\n");
+					toAskUser();
 				}
 				else
 				toGetUser_Number();
@@ -220,12 +255,17 @@ public class GuessTheNumber_Game {
   public void toCount_Random()
   {
 		/*
-		 * This method to count the appearance of random nummber and to store in an array.
-		 * If no number repeats then the count will be stored in respective index. If any number repeats then, the count will be stored in first repeated number's index then the repeated number's index will have 0.
-		 * As I started j as i index, bcauz it doesn't need to start from first as first comer's will check all the next places.
-		 * After first outer loop executes then it will check if the same number already came till before of that number, if it cames then that index will have 0 and it will not enter inner loop, to avoid the count collision.
-		 * For e.g => {1,2,1,1} -> 1 - count[0] : 3, 2 - count[1] : 1 ; if that while not given then the third place 1 will enter and inner loop and next number also 1, so it increases it's count as count[3] : 1.
-		 * As I don't want that to happen bcauz later i'll check user number's appearance with this counts.
+		 * This method to count the appearance of random number and to store in an array.
+		 * If no number repeats then the count will be stored in respective index. 
+		 * If any number repeats then, the count will be stored in first repeated number's index 
+		 * then the repeated number's index will have 0.
+		 * As I started j as i index, because it doesn't need to start from first as first comer's will check all the next places.
+		 * After first outer loop executes then it will check if the same number already came till before of that number, 
+		 * if it came then that index will have 0 and it will not enter inner loop, to avoid the count collision.
+		 * For e.g => {1,2,1,1} -> 1 - count[0] : 3, 2 - count[1] : 1 ; 
+		 * if that while not given then the third place 1 will enter into inner loop and next number also 1, 
+		 * so it increases it's count as count[3] : 1.
+		 * As I don't want that to happen because later i'll check user number's appearance with this counts.
 		 */
 	  count_random=new int[range];
 		
@@ -252,23 +292,11 @@ public class GuessTheNumber_Game {
 		}
   }
   
-  private int toGetCount(int num) {
-		// TODO Auto-generated method stub
-		  
-		  int i=0;
-		  for(;i<random_arr.length;i++)
-		  {
-			  if(num==random_arr[i])
-			  {
-				  return i;
-			  }
-		  }
-		  
-		return i;
-	}
-  
-  private void toUser_Count(int num, int[] count_user) {
-		// TODO Auto-generated method stub
+  private void toUser_Count(int num) {
+		/*
+		 * It will count the occurrence of user given number in an array, if any repeats then 
+		 * the count will be added in the first repeated position. 
+		 * */
 		  for(int i=0;i<user_arr.length;i++)
 		  {
 			  if(num==user_arr[i])
@@ -280,22 +308,65 @@ public class GuessTheNumber_Game {
 		  }
 	}
   
-  private int toFindNumber(int num) {
-		// TODO Auto-generated method stub
+  private int toFindcount(int num,int[] arr, int[] count_arr) {
+		/* It will give the count of occurrence of a number, the repeated occurrence are added in first repeated position, 
+		 * so it'll return the count of that position, if number not presents then it'll return 0. */
 		  int i=0;
-		  for(;i<user_arr.length;i++)
+		  for(;i<arr.length;i++)
 		  {
-			  if(num==user_arr[i])
-				  return i;
+			  if(num==arr[i])
+				  return count_arr[i];
 		  }
-		return i;
+		return 0;
 	}
+  
+  public void toAskUser()
+  {
+		try 
+		{
+			sc=new Scanner(System.in);
+			int choice=sc.nextInt();
+			switch(choice)
+			{
+				case 1:
+					System.out.println("This is the random number : "+random_number+" \nGood try, see you again.");
+					break;
+				case 2:
+					toGetUser_Number();
+					break;
+				default:
+					throw new GuessNumberException(Error_msg_GuessNumber.err3_choice_mismatch);
+			}
+		}
+		catch(InputMismatchException e)
+		{
+			e=new InputMismatchException(Error_msg_GuessNumber.err2_input_mismatch);
+			System.out.println(e.getMessage());
+			toAskUser();
+		}
+		catch(GuessNumberException e)
+		{
+			System.out.println(e.getMessage());
+			toAskUser();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+  }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("\n\n c - correct number and correct position \n p - correct number but position wrong \n x - wrong number\n");
 		GuessTheNumber_Game obj=new GuessTheNumber_Game();
-		obj.toGetRandom_Number();
-		obj.toGetUser_Number();	
+		try
+		{
+			obj.toGetRandom_Number();
+			obj.toGetUser_Number();	
+		}
+		finally {
+			obj.sc.close();
+		}
 	}
 
 }
